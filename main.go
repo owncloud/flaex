fda02bcf53ae8071ec37ad1139711942ce1604ed
Ilja Neumann <node512@gmail.com>
2020-03-23 16:10:16 +0100

    Add text-templating with sprig

--- main.go
+++ main.go
@@ -1,16 +1,42 @@
 package main
 
 import (
+	"flag"
+	"github.com/Masterminds/sprig"
+	"io/ioutil"
 	"log"
+	"os"
+	"text/template"
 )
 
 func main() {
 
-	opts, err := ParseFile("/home/ilja/code/ocis/ocis-konnectd/pkg/flagset/flagset.go")
+	var flagsetPath = flag.String("path", "", "Path to flagset file or directory")
+	var templatePath = flag.String("template", "flaex.tpl", "Path to go-template template file")
+	flag.Parse()
+	fi, err := os.Stat(*flagsetPath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Printf("%#v", opts)
+	if !fi.IsDir() {
+		tplContent, err := ioutil.ReadFile(*templatePath)
+		if err != nil {
+			log.Fatalf("unable to read template from %v: %v", *templatePath, err)
+		}
 
+		tpl, err := template.New("").Funcs(sprig.GenericFuncMap()).Parse(string(tplContent))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		opts, err := ParseFile(*flagsetPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if err := tpl.Execute(os.Stdout, opts); err != nil {
+			log.Fatal(err)
+		}
+	}
 }
